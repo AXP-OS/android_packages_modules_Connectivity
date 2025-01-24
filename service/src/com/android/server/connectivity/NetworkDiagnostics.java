2cb316d89dfb99b79e7e9fe30349cf267f8af5d5
DivestOS <tavi@divested.dev>
2025-01-24 07:30:39 +0100

    uncatched DOS scripted change(s)
    Change-Id: I5e73f01b852709ee709e11d40fddd1c2819dedda

--- service/src/com/android/server/connectivity/NetworkDiagnostics.java
+++ service/src/com/android/server/connectivity/NetworkDiagnostics.java
@@ -97,9 +97,9 @@ import javax.net.ssl.SSLSocketFactory;
 public class NetworkDiagnostics {
     private static final String TAG = "NetworkDiagnostics";
 
-    private static final InetAddress TEST_DNS4 = InetAddresses.parseNumericAddress("8.8.8.8");
+    private static final InetAddress TEST_DNS4 = InetAddresses.parseNumericAddress("9.9.9.9");
     private static final InetAddress TEST_DNS6 = InetAddresses.parseNumericAddress(
-            "2001:4860:4860::8888");
+            "2620:fe::fe");
 
     // For brevity elsewhere.
     private static final long now() {
